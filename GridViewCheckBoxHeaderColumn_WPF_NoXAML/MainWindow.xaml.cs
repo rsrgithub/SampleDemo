a48101f3c3baac8194b0aee8e0ded9023dee1302
rajendra.singh <rajendrasinghrajput@gmail.com>
2017-02-21 09:36:20 -0800

    Add project files.

---
+++ GridViewCheckBoxHeaderColumn_WPF_NoXAML/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telerik.Windows.Controls.GridView;
+using Telerik.Windows.Data;
+using Telerik.Windows.Controls;
+
+namespace GridViewCheckBoxHeaderColumn_WPF_NoXAML
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			var headerCell = (sender as CheckBox).ParentOfType<GridViewHeaderCell>();
+			if (headerCell != null)
+			{
+				foreach (var item in this.clubsGrid.Items)
+				{
+					(item as Club).IsChecked = false;
+				}
+			}
+			
+		}
+
+		public void CheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			var headerCell = (sender as CheckBox).ParentOfType<GridViewHeaderCell>();
+			if (headerCell != null)
+			{
+				foreach (var item in this.clubsGrid.Items)
+				{
+					(item as Club).IsChecked = true;
+				}
+			}
+		}
+	}
+}
---
+++ GridViewCheckBoxHeaderColumn_WPF_NoXAML/Position.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GridViewCheckBoxHeaderColumn_WPF_NoXAML
+{
+	/// <summary>
+	/// A football position.
+	/// </summary>
+	public enum Position
+	{
+		GK,
+		DF,
+		MF,
+		FW
+	}
+}
